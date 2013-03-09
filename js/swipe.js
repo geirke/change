89b0b29b4a1a5c884d91bb21becfc4a7c626d188
Geir Engdahl <geir.engdahl@gmail.com>
2013-03-09 15:36:52 +0100

    Swipe directions, change dn to down

--- js/swipe.js
+++ js/swipe.js
@@ -6,27 +6,26 @@ $.event.special.swipe.handleSwipe = function(start, stop) {
     page = $.mobile.activePage[0];
     var dx = start.coords[0] - stop.coords[0];
     var dy = start.coords[1] - stop.coords[1];
-    $.mobile.changePage("#flower");
 
     if (Math.abs(dx) > Math.abs(dy)) { //rl move
-	if (dx > 0) {
-	    if(page.dataset["right"]) {
-		$.mobile.changePage(page.dataset["right"]); //right
-	    }
-	} else {
-	    if(page.dataset["left"]) {
-		$.mobile.changePage(page.dataset["left"]); //left
-	    }
-	}
+		if (dx > 0) {
+		    if(page.dataset["right"]) {
+				$.mobile.changePage(page.dataset["right"], { transition: "slide" }); //right
+		    }
+		} else {
+		    if(page.dataset["left"]) {
+				$.mobile.changePage(page.dataset["left"], { transition: "slide", reverse: true }); //left
+		    }
+		}
     } else {
-	if (dy > 0) {
-	    if(page.dataset["down"]) {
-		$.mobile.changePage(page.dataset["down"]); //down
-	    }
-	} else {
-	    if(page.dataset["up"]) {
-		$.mobile.changePage(page.dataset["up"]); //up
-	    }
-	}
+		if (dy > 0) {
+		    if(page.dataset["down"]) {
+				$.mobile.changePage(page.dataset["down"], { transition: "slideup" }); //down
+		    }
+		} else {
+		    if(page.dataset["up"]) {
+				$.mobile.changePage(page.dataset["up"], { transition: "slidedown" }); //up
+		    }
+		}
     }
 }
