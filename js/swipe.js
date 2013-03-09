deb9f195af5db80763b76f815ee2cbdf0aa9d741
Stian Petlund <stian@1x-193-157-195-41.uio.no>
2013-03-09 19:07:29 +0100

    fixed superswipe

--- js/swipe.js
+++ js/swipe.js
@@ -1,6 +1,11 @@
+var mustReload = false;
 $(function() {
     $(document).bind("pagechange", function(event, obj) {
     	$('div.page').on('swipe', function() {});
+	if (mustReload) {
+	    mustReload = false;
+	    window.location.reload();
+	}
     });
     $('div.page').on('swipe', function() {});
 });
@@ -13,20 +18,26 @@ $.event.special.swipe.handleSwipe = function(start, stop) {
 	if (Math.abs(dx) > Math.abs(dy)) { //rl move
 	    if (dx > 0) {
 		if(page.dataset["right"]) {
+		    mustReload = true;
+		    console.log(page.dataset["right"]);
 		    $.mobile.changePage(page.dataset["right"], { reloadPage: true, transition: "slide" }); //right
 		}
 	    } else {
 		if(page.dataset["left"]) {
+		    mustReload = true;
+		    console.log(page.dataset["left"]);
 		    $.mobile.changePage(page.dataset["left"], { reloadPage: true, transition: "slide", reverse: true }); //left
 		}
 	    }
 	} else {
 	    if (dy > 0) {
 		if(page.dataset["down"]) {
+		    console.log(page.dataset["down"]);
 		    $.mobile.changePage(page.dataset["down"], { transition: "slideup" }); //down
 		}
 	    } else {
 		if(page.dataset["up"]) {
+		    console.log(page.dataset["up"]);
 		    $.mobile.changePage(page.dataset["up"], { transition: "slidedown" }); //up
 		}
 	    }
