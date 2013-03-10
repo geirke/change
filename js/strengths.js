f639a0f914c009993a780c41df9656892b034c21
Sigmund Hansen <sigmunha@ifi.uio.no>
2013-03-10 14:42:03 +0100

    Added text to profile pages for pre-programming and strength names.

--- js/strengths.js
+++ js/strengths.js
@@ -223,10 +223,19 @@ function fillText() {
     var categories = category.list();
 
     for (var i = 0; i < categories.length; ++i) {
+        var strength = strengths[categories[i]];
         var strategies = category.getStrategies(categories[i])
+        var whenthen = category.getWhenThen(categories[i]);
 
         for (var j = 0; j < strategies.length; ++j) {
             $('#strat' + j + '-' + i).text(strategies[j])
         }
+        
+        if (whenthen.length > 0) {
+            $('#when-' + i).text(whenthen[whenthen.length - 1].when);
+            $('#then-' + i).text(whenthen[whenthen.length - 1].then);
+        }
+        
+        $('.strength-' + i).text(strength.name);
     }
 }
