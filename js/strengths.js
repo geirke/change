8088fe2ac5240a9888e4c838392b0e15529b96dd
Sigmund Hansen <sigmunha@ifi.uio.no>
2013-03-10 09:22:32 +0100

    Removed errors from strength if strength is set incorrectly.

--- js/strengths.js
+++ js/strengths.js
@@ -57,46 +57,43 @@ var initstrengths = function() {
     strength = getURLParameter('strength');
     main = $('div#main');
 
-    if (strength !== undefined && !isNaN(strength = parseInt(strength))) {
-        // Set strength to an object from the strengths dictionary.
-        categoryList = category.list();
-        
-        if (categoryList.length === 0) {
-            mustReload = true;
-            $.mobile.changePage('profil.html', { transition: "slideleft" });
-        }
-        
-        if (strength < 0) {
-            strength = 0;
-        } else if (strength >= categoryList.length) {
-            strength = categoryList.length - 1;
-        }
-        
-        $('[data-role="page"]').each(function() {
-            if (strength === 0) {
-                $(this)[0].dataset['left'] = 'profil.html';
-            } else {
-                $(this)[0].dataset['left'] = 'strength.html?strength=' + (strength - 1);
-            }
-            
-            if (strength === categoryList.length - 1) {
-                $(this)[0].dataset['right'] = 'tekst.html';
-            } else {
-                $(this)[0].dataset['right'] = 'strength.html?strength=' + (strength + 1);
-            }
-        });
-        
-        strength = strengths[categoryList[strength]];
-        console.log(strength);
-        main.find('#about-header').html(strength.header);
-        main.find('#about').html(strength.about);
-
-        $('div[data-role="header"] h1').each(function() {
-            $(this).html(strength.name);
-        });
-
-    } else {
-        main.find('.ui-header').html("ERROR!");
-        main.find('#about').html("Something horrible happened!");
+    if (strength === undefined || isNaN(strength = parseInt(strength))) {
+        strength = 0;
     }
+    // Set strength to an object from the strengths dictionary.
+    categoryList = category.list();
+
+    if (categoryList.length === 0) {
+        mustReload = true;
+        $.mobile.changePage('profil.html', {transition: "slideleft"});
+    }
+
+    if (strength < 0) {
+        strength = 0;
+    } else if (strength >= categoryList.length) {
+        strength = categoryList.length - 1;
+    }
+
+    $('[data-role="page"]').each(function() {
+        if (strength === 0) {
+            $(this)[0].dataset['left'] = 'profil.html';
+        } else {
+            $(this)[0].dataset['left'] = 'strength.html?strength=' + (strength - 1);
+        }
+
+        if (strength === categoryList.length - 1) {
+            $(this)[0].dataset['right'] = 'tekst.html';
+        } else {
+            $(this)[0].dataset['right'] = 'strength.html?strength=' + (strength + 1);
+        }
+    });
+
+    strength = strengths[categoryList[strength]];
+    console.log(strength);
+    main.find('#about-header').html(strength.header);
+    main.find('#about').html(strength.about);
+
+    $('div[data-role="header"] h1').each(function() {
+        $(this).html(strength.name);
+    });
 };
